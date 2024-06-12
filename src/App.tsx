5a70daa68308bfa41561ecba73c82b13dea4c298
CollinsChacha <chachacollinsmugendi@gmail.com>
2024-06-12 10:17:07 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/SearchBar/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <h1>Weather app</h1>
+      <div></div>
+    </>
+  );
+}
+
+export default App;
